675c62b5cd756c5a171e02e5e908c16bc49ab3f4
caleb jephuneh <calebjephunneh3@gmail.com>
2022-12-08 23:36:32 +0300

    coode

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 function App() {
 
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Hero />
+    </>
     
   )
 }
