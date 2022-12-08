48727104c66b13fe56ebe93972d41bff66fb36a0
caleb jephuneh <calebjephunneh3@gmail.com>
2022-12-08 22:44:36 +0300

    comit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Navbar.css'
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Navbar = () => {
+
+const [click, setClick] = useState(false)
+const handleClick = () => setClick(!click)
+
   return (
     <div className='header'>
         <h1>De<span className='primary'>Fi</span></h1>
-        <ul className='nav-menu'>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li>
                 <a href='/'>Home</a>
             </li>
@@ -23,8 +28,8 @@ const Navbar = () => {
         <div className='btn-group'>
             <button className='btn'>Connect Wallet</button>
         </div>
-        <div className='hamburger'>
-            <MenuIcon />
+        <div className='hamburger' onClick={handleClick}>
+        {click ? (<CloseIcon size={20} style={{color: '#333'}}/>) : (<MenuIcon size={20} style={{color: '#333'}} />)}
         </div>
       
     </div>
